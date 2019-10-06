b78b7be2ca3649dbcfdf760fad8509aed4d5b1d7
parth <parth.27@outlook.com>
2019-10-05 22:21:13 -0500

    Fixed bug that causes seg fault

--- framebuffersocket.c
+++ framebuffersocket.c
@@ -6,10 +6,6 @@
 #include <linux/fb.h>
 #include <sys/mman.h>
 #include <sys/kd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <string.h>
-#include <netinet/in.h>
 #include <linux/input.h>
 
 int fb = 0;
@@ -26,18 +22,6 @@ double yScale = 1024.0;
 double xScale = 1280.0;
 double targetXRes;
 double targetYRes;
-
-//socket variables
-
-struct sockaddr_in serverAddress;
-int socket, client, readCount;
-unsigned char data[4096 + 1];
-
-// open socket
-
-int openSocket()
-{
-}
 
 int loadFrameBuffer()
 {
@@ -93,7 +77,7 @@ void disableConsoleGraphics()
 
 void loadKeyBoard()
 {
-	keyboard = open("/dev/input/event2", O_RDONLY);
+	keyboard = open("/dev/input/event4", O_RDONLY);
 }
 
 void closeKeyBoard()
@@ -103,7 +87,7 @@ void closeKeyBoard()
 
 void loadScale(double xRes, double yRes)
 {
-	printf("Loading Scale (%f, %f): ", xRes, yRes);
+	printf("Loading Scale (%f, %f): \n", xRes, yRes);
 	if (xRes < 1.0 || yRes < 1) 
 	{
 		printf("ERROR:Use proper scaling!\n");
@@ -114,7 +98,7 @@ void loadScale(double xRes, double yRes)
 		targetYRes = yRes;
 		xScale = (double) (vinfo.xres / xRes);
 		yScale = (double) (vinfo.yres / yRes);
-		printf("xRes = %f, yRes = %f, xScale = %f, yScale = %f\n");
+		printf("xRes = %f, yRes = %f, xScale = %f, yScale = %f\n", targetXRes, targetYRes, xScale, yScale);
 	}
 }
 
@@ -130,9 +114,9 @@ void drawPixel(int xPos, int yPos, int r, int g, int b, int a)
 	xPos *= xScale;
 	yPos *= yScale;
 	printf("Drawing pixel at: (%d, %d). RGB = (%d, %d, %d)\n", xPos, yPos, r, g, b);
-	for (int i = 0; i <= xScale; i++)
+	for (int i = 0; i < xScale; i++)
 	{
-		for (int j = 0; j <= yScale; j++)
+		for (int j = 0; j < yScale; j++)
 		{
 			location = (xPos + vinfo.xoffset + i) * (vinfo.bits_per_pixel/8) + (yPos + vinfo.yoffset + j) * finfo.line_length;
 			*(frameBuffer + location) = b;
@@ -146,6 +130,7 @@ void drawPixel(int xPos, int yPos, int r, int g, int b, int a)
 
 int main()
 {
+	printf("Starting framebuffer...\n");
 	//Redirect printf to log.txt - Overwrites last log file
 	freopen("log.txt", "w", stdout);
 	loadKeyBoard();
@@ -163,7 +148,7 @@ int main()
 	// Figure out the size of the screen in bytes
 	screensize = finfo.smem_len;
 	// Map the device to memory
-	frameBuffer = (char *)mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED, fb, 0);
+	frameBuffer = (unsigned char *)mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED, fb, 0);
 
 	if ((int)frameBuffer == -1) 
 	{
@@ -172,10 +157,10 @@ int main()
 	}
 	printf("The framebuffer device was mapped to memory successfully.\n");
 
-	loadScale(32.0, 32.0);
+	loadScale(64.0, 64.0);
 
 	srand(time(0));
-
+	printf("Starting to clear screen...\n");
 	for (y = 0; y < targetYRes; y++)
 	{
 		for (x = 0; x < targetXRes; x++)
@@ -186,7 +171,7 @@ int main()
 			drawPixel(x, y, r, g, b, 0);
 		}
 	}
-	
+	printf("Cleared Screen\n");
 	drawPixel(64, 64, 0, 0, 0, 0);
 
 	while (1)
